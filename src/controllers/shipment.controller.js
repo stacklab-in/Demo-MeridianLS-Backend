2736652ea989a70fcfe0dd3b266d19f43a7f1688
Sai Rathod <rsaiprasad4@gmail.com>
2024-02-21 12:02:49 +0530

    fix: Shipment Response modified

--- src/controllers/shipment.controller.js
+++ src/controllers/shipment.controller.js
@@ -40,18 +40,43 @@ const update = async (req, res) => {
 
 const getShipment = async (req, res) => {
     try {
-
         if (!req.body.trackingNumber) {
             return res.status(400).json({ error: 'Tracking Number is required' });
         }
+        const statusValueMap = {
+            "Pre Shipment Doc": "PreShipmentDoc",
+            "Picked Up": "PickedUp",
+            "In Transit Origin": "InTransitOriginPort",
+            "Clearance at Origin Port": "ClearanceatOriginPort",
+            "In Transit Overseas": "InTransitOverseas",
+            "Clearance at Destination Port": "ClearanceatDestinationPort",
+            "In Transit Destination": "InTransitDestination",
+            "Delivered": "Delivered"
+        };
+
         const shipment = await ShipmentTracking.findOne({ isDeleted: false, trackingNo: req.body.trackingNumber });
-        return res.status(200).json({ msg: 'Shipment fetched successfully!.', data: shipment });
+
+        if (!shipment) {
+            return res.status(404).json({ error: 'Shipment not found' });
+        }
+
+        let modifiedShipment = { ...shipment.toObject() }; // Clone the shipment object to avoid mutation
+
+        if (modifiedShipment.statuses && modifiedShipment.statuses.length > 0) {
+            modifiedShipment.statuses = modifiedShipment.statuses.map((status, index) => {
+                const mappedValue = statusValueMap[status.value] || status.value; // Get the mapped value or original value if not found
+                console.log("🚀 ~ modifiedShipment.statuses=modifiedShipment.statuses.map ~ mappedValue:", mappedValue)
+                return { ...status, value: mappedValue, stageId: index }; // Adding stageId to each status object
+            })
+        }
+
+        console.log(modifiedShipment);
+        return res.status(200).json({ msg: 'Shipment fetched successfully!', data: modifiedShipment });
     } catch (error) {
         console.log(error);
         serverLogger("error", { error: error.stack || error.toString() });
         return res.status(500).json({ error: 'Internal Server Error' });
     }
-
 };
 
 

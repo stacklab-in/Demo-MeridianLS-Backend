fd85176c970d4fd4de2d1455993a60b36201a775
Sai Rathod <rsaiprasad4@gmail.com>
2024-03-21 17:34:41 +0530

    fix: Bug fixed for lead add

--- src/models/schemas/Inquiry.js
+++ src/models/schemas/Inquiry.js
@@ -3,42 +3,60 @@ const { Schema, model } = require("mongoose");
 const inquirySchema = new Schema({
   commodity: {
     type: String,
-    required: true
+    // required: true
   },
   hsCode: {
     type: String,
-    required: true
+    // required: true
   },
   shipmentTerm: {
     type: String,
-    required: true
+    // required: true
   },
   mode: {
     type: String,
-    required: true
+    // required: true
   },
   typeOfPacking: {
     type: String,
-    required: true
+    // required: true
   },
   numberOfPackages: {
     type: Number,
-    required: true
+    // required: true
   },
   dimensions: [
     {
-      qty: { type: Number, required: true },
-      length: { type: Number, required: true },
-      breadth: { type: Number, required: true },
-      height: { type: Number, required: true },
+      qty: {
+        type: Number,
+        // required: true
+      },
+      length: {
+        type: Number,
+        // required: true
+      },
+      breadth: {
+        type: Number,
+        // required: true
+      },
+      height: {
+        type: Number,
+        // required: true
+      },
+      netWeight: {
+        type: Number,
+        // required: true
+      },
+      grossWeight: {
+        type: Number,
+        // required: true
+      },
     }],
   netWeight: {
     type: Number,
-    required: true
   },
   grossWeight: {
     type: Number,
-    required: true
   },
   deliveryAddressCode: {
     type: Number,
